94542e36353f5a728ae9baf9eb48981cb98d3604
ajmal31 <ajmalmuhammed846@gmail.com>
2024-04-01 02:12:31 +0530

    jeje

--- git.js
+++ git.js
@@ -3,6 +3,10 @@ console.log("helo ima ajmalllllllll")
 console.log("ima pull")
 console.log("im fetch")
 
+<<<<<<< Updated upstream
 console.log("git stach 3rd")
 console.log("secons stach")
 console.log("secons stach")
+=======
+console.log("secons stach")
+>>>>>>> Stashed changes
